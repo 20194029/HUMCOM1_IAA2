6432fb31822d099d5d9ca1cefec8b8e17def897d
Ricardo Rochel Patrick B. Vogel/ HUMCOM1_20194029 <98304263+RicardoRochelPatrickVogel@users.noreply.github.com>
2022-05-13 21:01:27 +0800

    Add files via upload

---
+++ Exercise 15/ex15.js
@@ -0,0 +1,15 @@
+
+
+var person = "Ricardo Rochel Patrick B. Vogel";
+    var age = '23';
+    var phone = "09955740633";
+    var email = "Patrickvogel344@yahoo.com";
+    var address = "168, Donalutgarda, Cut-Cut1st, Capas, Tarlac";
+    var prog = "Information Technology";
+    var id = "20194029";
+    var yr = "2nd";
+    var sec = "IAA2";
+    
+    
+
+    
